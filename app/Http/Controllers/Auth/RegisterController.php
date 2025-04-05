c7ba0a2279a590bf9cf9ef256eff898f0851fda2
mosyhub <alvinsymo@gmail.com>
2025-04-05 17:00:35 +0800

    fix user register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,16 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Storage;
-
-class RegisterController extends Controller
-{
-    use RegistersUsers;
-
-    protected $redirectTo = '/home';
-
-=======
 
 class RegisterController extends Controller
 {
@@ -45,42 +36,24 @@ class RegisterController extends Controller
      *
      * @return void
      */
->>>>>>> 0ccc2e41ec5bd0c2ba7c2d1ebc143c2df30d1264
     public function __construct()
     {
         $this->middleware('guest');
     }
 
-<<<<<<< HEAD
-=======
     /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
->>>>>>> 0ccc2e41ec5bd0c2ba7c2d1ebc143c2df30d1264
     protected function validator(array $data)
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-<<<<<<< HEAD
-            'profile_picture' => ['nullable', 'image', 'max:2048'], // ✅ validate image
-        ]);
-    }
-
-    protected function create(array $data)
-    {
-        $profilePicturePath = null;
-
-        // ✅ Check and upload profile picture if exists
-        if (request()->hasFile('profile_picture')) {
-            $profilePicturePath = request()->file('profile_picture')->store('profile_pictures', 'public');
-        }
-
-=======
+            'profile_picture' => ['nullable', 'image', 'max:2048'], // Validate image
         ]);
     }
 
@@ -92,15 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
->>>>>>> 0ccc2e41ec5bd0c2ba7c2d1ebc143c2df30d1264
+        $profilePicturePath = null;
+
+        // Check and upload profile picture if exists
+        if (request()->hasFile('profile_picture')) {
+            $profilePicturePath = request()->file('profile_picture')->store('profile_pictures', 'public');
+        }
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-<<<<<<< HEAD
-            'profile_picture' => $profilePicturePath, // ✅ save file path
-=======
->>>>>>> 0ccc2e41ec5bd0c2ba7c2d1ebc143c2df30d1264
+            'profile_picture' => $profilePicturePath, // Save file path
         ]);
     }
 }
